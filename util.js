63634646469efb4cdd061380eba1b0cff03f3872
Fernando Pessoa <fernando.pessoa@segment.net.br>
2022-07-10 15:45:24 -0300

    incluido virtual service #1

--- util.js
+++ util.js
@@ -352,7 +352,7 @@ class Util {
                 let lines = str.split('\n');
                 lines.forEach(line => {
                    
-                    if(!(line.indexOf(begin_mark) >= 0 || line.indexOf(end_mark) >= 0 || line.length < 1)) {
+                    if(!(line.indexOf(begin_mark) >= 0 || line.indexOf(end_mark) >= 0 || line.length < 2)) {
                         console.log('line:' + line);
                     }
         

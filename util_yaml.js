8457d32a53368181b021d6b2b11aa408ecf9378b
Fernando Pessoa <fernando.pessoa@segment.net.br>
2022-07-11 17:12:38 -0300

    incluido yaml

--- util_yaml.js
+++ util_yaml.js
@@ -20,6 +20,22 @@ class YAML {
         return path;
         
     }
+
+    remove_item(key,vars) {
+        doc.services.nodered.environment
+        var index = 0;
+        var found = -1;
+        vars.forEach(element => {
+            if(element.indexOf(key) >= 0) {
+                found = index;
+            }
+            index++;
+        });
+
+        if(found >=0 ) {
+            vars.splice(found,1);
+        }
+    }
     /**
      * 
      * @param {*} env 
@@ -38,25 +54,27 @@ class YAML {
 
                     const doc = yaml.load(fs.readFileSync(path, 'utf8'));
                     console.log(doc);
-                    var keys = [];
-                    doc.services.nodered.environment.forEach(e => {
-                        console.log(e)
-                        keys.push(e.split("=")[0].replace("-","").trim());
-                        console.log(keys);
+                    // var keys = [];
+                    // doc.services.nodered.environment.forEach(e => {
+                    //     console.log(e)
+                    //     keys.push(e.split("=")[0].replace("-","").trim());
+                    //     console.log(keys);
 
-                    });
+                    // });
         
                     //doc.services.nodered.environment = [];
                     vars.forEach(v => {
                         var line = `${v.p} = ${v.to}`;
-                        var data = doc.services.nodered.environment.filter(item => item.indexOf(v.p) >= 0);
-                        if(data) {
-                            console.log(`found: ${data}`)
-                            const index =doc.services.nodered.environment.indexOf(data);
-                            console.log(`found: ${index}`)
-                            if(index >=0 )
-                                doc.services.nodered.environment.splice(index,1);
-                        }
+                        this.remove_item(v.p,doc.services.nodered.environment);
+
+                        // var data = doc.services.nodered.environment.filter(item => item.indexOf(v.p) >= 0);
+                        // if(data) {
+                        //     console.log(`found: ${data}`)
+                        //     const index =doc.services.nodered.environment.indexOf(data);
+                        //     console.log(`found: ${index}`)
+                        //     if(index >=0 )
+                        //         doc.services.nodered.environment.splice(index,1);
+                        // }
                         // const index = keys.indexOf(v.p);
                         // if(index >= 0) {
                         //     doc.services.nodered.environment.splice(index,1);
